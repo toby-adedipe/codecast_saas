7ff1465cc46d2fe676ba31b20db9b39b432a43f1
toby adedipe <toby_adedipe@yahoo.com>
2017-10-11 16:06:00 +0000

    saved comments to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,18 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
         
+        if @contact.save
+            redirect_to new_contact_path, notice: "Message Sent"
+        else
+            redirect_to new_contact_path, notice: "Message not Sent"
+        end
     end
     
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
